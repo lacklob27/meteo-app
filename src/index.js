fe5269f202ccf6d4822bcf06d349bfbbf21203f8
Lauren Klobuchar <156728890+lacklob27@users.noreply.github.com>
2024-01-21 17:39:31 -0600

    Injected Additional Weather Data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,49 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = capitalizeWords(
+    response.data.condition.description
+  );
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let roundedWindSpeed = Math.round(response.data.wind.speed);
+  windSpeedElement.innerHTML = `${roundedWindSpeed} mph`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  hours = hours % 12 || 12;
+  let amPm = hours >= 12 ? "AM" : "PM";
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes} ${amPm}`;
+}
+
+function capitalizeWords(str) {
+  return str.replace(/\b\w/g, (match) => match.toUpperCase());
 }
 
 function searchCity(city) {
