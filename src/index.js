f649d23f92564c692652a8fe7b5c21b0f839c190
Lauren Klobuchar <156728890+lacklob27@users.noreply.github.com>
2024-01-22 11:01:59 -0600

    Added Icons & Refined JS Functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
-function updateIconBasedOnDescription(description) {
+function updateIcon(response) {
   const iconElement = document.querySelector("#weather-app-icon-image");
+  const description = response.data.condition.description.toLowerCase();
 
-  switch (description.toLowerCase()) {
+  switch (description) {
     case "clear sky":
-      iconElement.innerHTML = `<img src="assets/ClearSky.png" alt="Clear Sky" class="weather-app-icon-image" />`;
+      iconElement.src = "assets/ClearSkyDay.png";
+      iconElement.alt = "Clear Sky";
+      break;
+    case "few clouds":
+      iconElement.src = "assets/FewCloudsDay.png";
+      iconElement.alt = "Few Clouds";
+      break;
+    case "scattered clouds":
+      iconElement.src = "assets/ScatteredCloudsDay.png";
+      iconElement.alt = "Scattered Clouds";
+      break;
+    case "overcast clouds":
+      iconElement.src = "assets/ScatteredCloudsDay.png";
+      iconElement.alt = "Overcast Clouds";
+      break;
+    case "broken clouds":
+      iconElement.src = "assets/BrokenCloudsDay.png";
+      iconElement.alt = "Broken Clouds";
+      break;
+    case "shower rain":
+      iconElement.src = "assets/ShowerRainDay.png";
+      iconElement.alt = "Shower Rain";
+      break;
+    case "rain":
+      iconElement.src = "assets/RainDay.png";
+      iconElement.alt = "Rain";
+      break;
+    case "moderate rain":
+      iconElement.src = "assets/RainDay.png";
+      iconElement.alt = "Moderate Rain";
+      break;
+    case "light rain":
+      iconElement.src = "assets/RainDay.png";
+      iconElement.alt = "Light Rain";
+      break;
+    case "thunderstorm":
+      iconElement.src = "assets/ThunderstormDay.png";
+      iconElement.alt = "Thunderstorm";
+      break;
+    case "snow":
+      iconElement.src = "assets/SnowDay.png";
+      iconElement.alt = "Snow";
+      break;
+    case "mist":
+      iconElement.src = "assets/MistDay.png";
+      iconElement.alt = "Mist";
+      break;
+    case "windy":
+      iconElement.src = "assets/WindyDay.png";
+      iconElement.alt = "Windy";
       break;
   }
 }
@@ -23,7 +73,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = capitalizeWords(
     response.data.condition.description
   );
-  updateIconBasedOnDescription(response.data.condition.description);
+  updateIcon(response);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   temperatureElement.innerHTML = Math.round(temperature);
 
@@ -35,15 +85,15 @@ function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   hours = hours % 12 || 12;
-  let amPm = hours >= 12 ? "AM" : "PM";
+  let amPm = hours >= 12 ? "PM" : "AM";
   let days = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
   let day = days[date.getDay()];
 
